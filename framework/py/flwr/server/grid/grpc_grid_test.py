8fd0623f392283372267694e1cd43950ea91ea8f
RabeyaMuna <rabeyakhatun.muna@mail.concordia.ca>
2026-09-12 14:01:48 -0400

    RabeyaMuna__diff__id_104

--- framework/py/flwr/server/grid/grpc_grid_test.py
+++ framework/py/flwr/server/grid/grpc_grid_test.py
@@ -14,7 +14,6 @@
 # ==============================================================================
 """Tests for grid SDK."""
 
-
 import time
 import unittest
 from unittest.mock import Mock, patch
@@ -32,7 +31,6 @@ from flwr.proto.appio_pb2 import (  # pylint: disable=E0611
     PushAppMessagesRequest,
 )
 from flwr.proto.message_pb2 import ObjectIDs  # pylint: disable=E0611
-from flwr.proto.message_pb2 import ObjectTree  # pylint: disable=E0611
 from flwr.proto.run_pb2 import (  # pylint: disable=E0611
     GetRunRequest,
     GetRunResponse,
@@ -220,7 +218,7 @@ class TestGrpcGrid(unittest.TestCase):
         reply.metadata.__dict__["_message_id"] = reply.object_id
         self.mock_stub.PullMessages.return_value = Mock(
             messages_list=[message_to_proto(reply)],
-            get_object_tree(reply),
+            message_object_trees=[get_object_tree(reply)],
         )
         self.mock_stub.PullObject.return_value = Mock(
             object_found=True, object_available=True, object_content=reply.deflate()
